f1b1bf2978be3916d04e7f720afa5285132393f9
Pietro Faraci <198417026+pietro1644@users.noreply.github.com>
2025-03-10 22:00:41 -0400

    displayed cards on screen

--- cards.js
+++ cards.js
@@ -2,18 +2,28 @@ const SUITS = ["swords", "cups", "coins", "clubs"];
 const VALUES = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 function createDeck() {
-
-  let deck = []
+  let deck = [];
 
   for (let i = 0; i < SUITS.length; i++) {
     for (let n = 0; n < VALUES.length; n++) {
-      let card = {suit: SUITS[i], value: VALUES[n]} 
-      deck.push(card)
-
+      let card = { suit: SUITS[i], value: VALUES[n] };
+      deck.push(card);
     }
   }
-  return deck
+  return deck;
 }
 
-deckOfCards = createDeck();
-console.log(deckOfCards)
+function printCards() {
+  const container = document.querySelector(".card-container");
+
+  for (let singleCard of createDeck()) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    card.textContent = singleCard.value + singleCard.suit;
+    console.log(singleCard);
+    container.appendChild(card);
+  }
+}
+
+printCards();
