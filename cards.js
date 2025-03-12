990133ceef6c1504d0423fed914216a01981a79c
Pietro Faraci <198417026+pietro1644@users.noreply.github.com>
2025-03-12 00:00:35 -0400

    shuffle deck working

--- cards.js
+++ cards.js
@@ -13,10 +13,10 @@ function createDeck() {
   return deck;
 }
 
-function printCards() {
+function printCards(deck) {
   const container = document.querySelector(".card-container");
 
-  for (let singleCard of createDeck()) {
+  for (let singleCard of deck) {
     const card = document.createElement("div");
 
     if (singleCard.suit == "swords") {
@@ -35,4 +35,22 @@ function printCards() {
   }
 }
 
-printCards();
+function shuffle(deck) {
+  for (let i = 0; i < 1000; i++) {
+    let location1 = Math.floor((Math.random() * deck.length));
+    let location2 = Math.floor((Math.random() * deck.length));
+    let tmp = deck[location1]
+
+
+    deck[location1] = deck[location2];
+    deck[location2] = tmp;
+  }
+  return deck
+}
+
+mainDeck = createDeck();
+
+shuffle(mainDeck);
+
+printCards(mainDeck);
+
