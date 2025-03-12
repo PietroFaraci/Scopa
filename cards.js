ade3228217880886ca61c3ac88f4e345bde867c2
Pietro Faraci <198417026+pietro1644@users.noreply.github.com>
2025-03-11 23:27:35 -0400

    added color to cards

--- cards.js
+++ cards.js
@@ -19,22 +19,17 @@ function printCards() {
   for (let singleCard of createDeck()) {
     const card = document.createElement("div");
 
-
     if (singleCard.suit == "swords") {
-      card.classList.add("swords-card")
-    }
-    else if (singleCard.suit == "cups") {
-      card.classList.add("cups-card")
-    }
-    else if (singleCard.suit == "coins") {
-      card.classList.add("coins-card")
-    }
-    else if (singleCard.suit == "clubs") {
-      card.classList.add("clubs-card")
+      card.classList.add("swords-card");
+    } else if (singleCard.suit == "cups") {
+      card.classList.add("cups-card");
+    } else if (singleCard.suit == "coins") {
+      card.classList.add("coins-card");
+    } else if (singleCard.suit == "clubs") {
+      card.classList.add("clubs-card");
     }
 
-
-    card.textContent = singleCard.value + ' ' + singleCard.suit;
+    card.textContent = singleCard.value + " " + singleCard.suit;
     console.log(singleCard);
     container.appendChild(card);
   }
