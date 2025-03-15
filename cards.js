122ee6d2fb054ee0b041d027366e31fab21d8bb2
Pietro Faraci <198417026+pietro1644@users.noreply.github.com>
2025-03-15 06:28:31 -0400

    ran func, prep for features

--- cards.js
+++ cards.js
@@ -12,7 +12,6 @@ function createDeck() {
   }
   return deck;
 }
-
 function printCards(deck) {
   const container = document.querySelector(".card-container");
 
@@ -34,23 +33,25 @@ function printCards(deck) {
     container.appendChild(card);
   }
 }
-
 function shuffle(deck) {
   for (let i = 0; i < 1000; i++) {
-    let location1 = Math.floor((Math.random() * deck.length));
-    let location2 = Math.floor((Math.random() * deck.length));
-    let tmp = deck[location1]
-
+    let location1 = Math.floor(Math.random() * deck.length);
+    let location2 = Math.floor(Math.random() * deck.length);
+    let tmp = deck[location1];
 
     deck[location1] = deck[location2];
     deck[location2] = tmp;
   }
-  return deck
+  return deck;
 }
 
-mainDeck = createDeck();
+let mainDeck = createDeck();
+printCards(shuffle(mainDeck));
 
-shuffle(mainDeck);
+function printUserHand() {
+  // function to print 3 cards at the BOS for the user
+}
 
-printCards(mainDeck);
-
+function printComputerHand() {
+  // function to print 3 cards at TOS for the computer
+}
