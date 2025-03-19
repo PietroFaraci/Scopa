99de1930b541c1c651419c6f862136e7b82fb13e
Pietro Faraci <198417026+pietro1644@users.noreply.github.com>
2025-03-19 14:56:58 -0400

    latest save

--- logic.js
+++ logic.js
@@ -1,4 +1,9 @@
 // this file will be for determining what function to run depending on user input
 
+const startButton = document.querySelector(".start-game")
+// const resumeButton = document.querySelector(".resume-game")
 
-
+startButton.addEventListener("click",() => {
+  console.log("clicked start button")
+  window.location.href = "./pages/round.html"
+})
